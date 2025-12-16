f33e6d1ff2afbb23170adad523315e0f2de727e3
Bartłomiej Pawłowski <bpawlowski91@gmail.com>
2025-12-16 20:33:26 +0100

    Add: heiConfig to ModuleCsv

--- src/contexts/heiConfig.tsx
+++ src/contexts/heiConfig.tsx
@@ -1,5 +1,5 @@
 export const heiConfig = {
-    thisHeiEC: import.meta.env.VITE_THIS_HEI_EC || 'PL_LUBLIN02',
+    thisHeiEC: import.meta.env.VITE_THIS_HEI_EC || 'PL LUBLIN02',
     thisHeiNameCsv: import.meta.env.VITE_THIS_HEI_NAME_CSV || 'KATOLICKI UNIWERSYTET LUBELSKI JANA PAWLA II',
     thisHeiNameOrg: import.meta.env.VITE_THIS_HEI_NAME_ORG || 'Katolicki Uniwesytet Lubelski Jana Pawła II',
     thisHeiNameEn: import.meta.env.VITE_THIS_HEI_NAME_EN || 'The John Paul II Catholic University of Lublin',
