8ef7418d2c1a81608e69f6d5674b99a6a09f64e6
Bartłomiej Pawłowski <bpawlowski91@gmail.com>
2025-12-16 19:36:44 +0100

    Add: this HEI config in .env and heiConfig.tsx

---
+++ src/contexts/heiConfig.tsx
@@ -0,0 +1,5 @@
+export const heiConfig = {
+    thisHeiEC: import.meta.env.VITE_THIS_HEI_EC || 'PL_LUBLIN02',
+    thisHeiName: import.meta.env.VITE_THIS_HEI_NAME || 'KATOLICKI UNIWERSYTET LUBELSKI JANA PAWLA II',
+    thisHeiDomain: import.meta.env.VITE_THIS_HEI_DOMAIN || 'kul.pl',
+};
