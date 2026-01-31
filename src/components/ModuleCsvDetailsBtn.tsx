bd20eca64b1056ce573217675af55670834980be
Bartłomiej Pawłowski <bpawlowski91@gmail.com>
2026-01-31 13:56:46 +0100

    Add Link to Partner's website in ModuleCsvDetailsBtn

--- src/components/ModuleCsvDetailsBtn.tsx
+++ src/components/ModuleCsvDetailsBtn.tsx
@@ -1,8 +1,9 @@
-import { Box, Button, Tooltip, Typography } from "@mui/material";
+import { Box, Button, Tooltip, Typography, useTheme, lighten, darken, Link } from "@mui/material";
 import { useTranslation } from 'react-i18next';
 import { useModalContext } from '../contexts/ModalContext';
 import { useThemeContext } from '../contexts/ThemeContext';
 import ContentPasteSearchIcon from '@mui/icons-material/ContentPasteSearch';
+import LinkIcon from '@mui/icons-material/Link';
 import alasql from 'alasql';
 import { TFunction } from "i18next";
 
@@ -30,6 +31,7 @@ function formatEqfValue(value: string, t: TFunction): string {
 
 function ModuleCsvDetailsBtn({ data, rowId }: ModuleCsvDetailsBtnProps) {
   const { t } = useTranslation();
+  const theme = useTheme();
   const { modalOpen } = useModalContext();
   const { mode } = useThemeContext();
 
@@ -41,111 +43,124 @@ function ModuleCsvDetailsBtn({ data, rowId }: ModuleCsvDetailsBtnProps) {
 
     // XLSX COLUMNS USED IN DETAILED VIEW GO HERE (except id, which is defined as rowId)
     const {
-        CSVTH_ERASMUS_CODE,
-        CSVTH_INSTITUTION_NAME,
-        CSVTH_MOBILITY_TYPE,
-        CSVTH_EQF,
-        CSVTH_BLENDED,
-        CSVTH_LANGUAGE_REQUIREMENTS,
-        CSVTH_FROM,
-        CSVTH_TO,
+      CSVTH_ERASMUS_CODE,
+      CSVTH_INSTITUTION_NAME,
+      CSVTH_MOBILITY_TYPE,
+      CSVTH_EQF,
+      CSVTH_BLENDED,
+      CSVTH_LANGUAGE_REQUIREMENTS,
+      CSVTH_FROM,
+      CSVTH_TO,
+      CSVTH_PARTNER_WWW,
     } = selectedRow;
 
     // MODAL CONTENT
     const content = (
-        
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 0.5 }}>
+
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 0.5 }}>
 
         {/* HEADER - MOBILITY TYPE */}
         <Typography sx={{ mb: 1, textDecoration: 'underline', textAlign: 'center', fontSize: '0.95em' }}>
-            {t(CSVTH_MOBILITY_TYPE)}
+          {t(CSVTH_MOBILITY_TYPE)}
         </Typography>
 
         {/* SENDING INSTITUTION */}
         <Box sx={{ textAlign: 'center', mb: 1 }}>
-        <Typography sx={{ fontSize: '0.95em', fontWeight: 'bold' }}>
+          <Typography sx={{ fontSize: '0.95em', fontWeight: 'bold' }}>
             {t('EWP_SENDING_INSTITUTION')}:
-        </Typography>
-        <Typography sx={{ fontSize: '0.95em' }}>
+          </Typography>
+          <Typography sx={{ fontSize: '0.95em' }}>
             {CSVTH_MOBILITY_TYPE?.includes('OUTGOING')
-            ? `${t('THIS_INSTITUTION_NAME').replace(/ /g, '\u00A0')} (${t('THIS_INSTITUTION_EC').replace(/ /g, '\u00A0')})`
-            : CSVTH_MOBILITY_TYPE?.includes('INCOMING')
-            ? `${CSVTH_INSTITUTION_NAME.replace(/ /g, '\u00A0')} (${CSVTH_ERASMUS_CODE.replace(/ /g, '\u00A0')})`
-            : '-'}
-        </Typography>
+              ? `${t('THIS_INSTITUTION_NAME').replace(/ /g, '\u00A0')} (${t('THIS_INSTITUTION_EC').replace(/ /g, '\u00A0')})`
+              : CSVTH_MOBILITY_TYPE?.includes('INCOMING')
+                ? `${CSVTH_INSTITUTION_NAME.replace(/ /g, '\u00A0')} (${CSVTH_ERASMUS_CODE.replace(/ /g, '\u00A0')})`
+                : '-'}
+          </Typography>
         </Box>
 
         {/* RECEIVING INSTITUTION */}
         <Box sx={{ textAlign: 'center', mb: 1 }}>
-        <Typography sx={{ fontSize: '0.95em', fontWeight: 'bold' }}>
+          <Typography sx={{ fontSize: '0.95em', fontWeight: 'bold' }}>
             {t('EWP_RECEIVING_INSTITUTION')}:
-        </Typography>
-        <Typography sx={{ fontSize: '0.95em' }}>
+          </Typography>
+          <Typography sx={{ fontSize: '0.95em' }}>
             {CSVTH_MOBILITY_TYPE?.includes('OUTGOING')
-            ? `${CSVTH_INSTITUTION_NAME.replace(/ /g, '\u00A0')} (${CSVTH_ERASMUS_CODE.replace(/ /g, '\u00A0')})`
-            : CSVTH_MOBILITY_TYPE?.includes('INCOMING')
-            ? `${t('THIS_INSTITUTION_NAME').replace(/ /g, '\u00A0')} (${t('THIS_INSTITUTION_EC').replace(/ /g, '\u00A0')})`
-            : '-'}
-        </Typography>
+              ? `${CSVTH_INSTITUTION_NAME.replace(/ /g, '\u00A0')} (${CSVTH_ERASMUS_CODE.replace(/ /g, '\u00A0')})`
+              : CSVTH_MOBILITY_TYPE?.includes('INCOMING')
+                ? `${t('THIS_INSTITUTION_NAME').replace(/ /g, '\u00A0')} (${t('THIS_INSTITUTION_EC').replace(/ /g, '\u00A0')})`
+                : '-'}
+          </Typography>
         </Box>
 
         {/* formatted EQF */}
         <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography variant="body2" sx={{ fontWeight: 'bold', mr: 1 }}>
+          <Typography variant="body2" sx={{ fontWeight: 'bold', mr: 1 }}>
             {t('CSVTH_EQF')}:
-        </Typography>
-        <Typography variant="body2">{formatEqfValue(CSVTH_EQF, t) ?? '-'}</Typography>
+          </Typography>
+          <Typography variant="body2">{formatEqfValue(CSVTH_EQF, t) ?? '-'}</Typography>
         </Box>
 
         {/* IF BLENDED */}
         <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography variant="body2" sx={{ fontWeight: 'bold', mr: 1 }}>
+          <Typography variant="body2" sx={{ fontWeight: 'bold', mr: 1 }}>
             {t('CSVTH_BLENDED')}:
-        </Typography>
-        <Typography
-          variant="body2"
-          sx={{
-            color:
-              CSVTH_BLENDED === "CSVTD_YES" // IF BLENDED YES
-                ? mode === "light"
-                  ? "green" // light theme darkgreen
-                  : "lightgreen" // dark theme lightgreen
-                : CSVTH_BLENDED === "CSVTD_NO" // IF BLENDED NO
-                ? mode === "light"
-                  ? "red" // light theme darkred
-                  : "#f28b82" // dark theme lightred
-                : "inherit",
-          }}
-        >
-          {t(CSVTH_BLENDED) ?? "-"}
-        </Typography>
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{
+              color:
+                CSVTH_BLENDED === "CSVTD_YES" // IF BLENDED YES
+                  ? mode === "light"
+                    ? "green" // light theme darkgreen
+                    : "lightgreen" // dark theme lightgreen
+                  : CSVTH_BLENDED === "CSVTD_NO" // IF BLENDED NO
+                    ? mode === "light"
+                      ? "red" // light theme darkred
+                      : "#f28b82" // dark theme lightred
+                    : "inherit",
+            }}
+          >
+            {t(CSVTH_BLENDED) ?? "-"}
+          </Typography>
         </Box>
 
         <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography variant="body2" sx={{ fontWeight: 'bold', mr: 1 }}>
+          <Typography variant="body2" sx={{ fontWeight: 'bold', mr: 1 }}>
             {t('CSVTH_LANGUAGE_REQUIREMENTS')}:
-        </Typography>
-        <Typography variant="body2">{t(CSVTH_LANGUAGE_REQUIREMENTS) ?? '-'}</Typography>
+          </Typography>
+          <Typography variant="body2">{t(CSVTH_LANGUAGE_REQUIREMENTS) ?? '-'}</Typography>
         </Box>
 
         <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography variant="body2" sx={{ fontWeight: 'bold', mr: 1 }}>
+          <Typography variant="body2" sx={{ fontWeight: 'bold', mr: 1 }}>
             {t('CSVTH_FROM')}:
-        </Typography>
-        <Typography variant="body2">
+          </Typography>
+          <Typography variant="body2">
             {CSVTH_FROM instanceof Date ? CSVTH_FROM.toLocaleDateString() : CSVTH_FROM ?? '-'}
-        </Typography>
+          </Typography>
         </Box>
 
         <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography variant="body2" sx={{ fontWeight: 'bold', mr: 1 }}>
+          <Typography variant="body2" sx={{ fontWeight: 'bold', mr: 1 }}>
             {t('CSVTH_TO')}:
-        </Typography>
-        <Typography variant="body2">
+          </Typography>
+          <Typography variant="body2">
             {CSVTH_TO instanceof Date ? CSVTH_TO.toLocaleDateString() : CSVTH_TO ?? '-'}
-        </Typography>
+          </Typography>
         </Box>
-    </Box>
+
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 1, alignItems: 'center' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <LinkIcon sx={{ mr: 0.7, fontSize: 16 }} />
+            <Typography variant="body2" sx={{ fontWeight: 'bold', mr: 1, display: 'flex', alignItems: 'center' }}>
+              {t('CSVTH_PARTNER_WWW')}:
+            </Typography>
+          </Box>
+          <Link href={"https://" + CSVTH_PARTNER_WWW} target="_blank" rel="noopener noreferrer" variant="body2" sx={{ color: mode === 'light' ? darken(theme.palette.primary.main, 0.22) : lighten(theme.palette.primary.main, 0.3), cursor: 'pointer', display: 'flex', alignItems: 'center', textDecoration: 'none', '&:hover': { textDecoration: 'underline', color: mode === 'light' ? darken(theme.palette.primary.main, 0.22) : lighten(theme.palette.primary.main, 0.3) } }}>
+            {CSVTH_PARTNER_WWW ?? '-'}
+          </Link>
+        </Box>
+      </Box>
 
     );
 
